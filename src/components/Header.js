c2c2e6a730c42f12bc018c5ff412323eb56ba1dd
Jessica Moore <jessica.cmoore1@gmail.com>
2019-08-11 16:00:50 -0400

    Form styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,17 +8,20 @@ const Header = ({handleRegionChange}) => {
       <div className="wrapper">
         <h1>Discover the Great Wines<br/>of the Niagara Region.</h1>
         <form onSubmit={(e) => handleRegionChange(e, regionEl.current.value)}>
-          <label htmlFor="region-select">Region:</label>
-          <select id="region-select" ref={regionEl}>
-            <option value="baseRegion">Niagara Region</option>
-            <option value="beamsville">Beamsville</option>
-            <option value="jordanStn">Jordan Station</option>
-            <option value="lincoln">Lincoln</option>
-            <option value="niagaraFalls">Niagara Falls</option>
-            <option value="notl">Niagara-On-The-Lake</option>
-            <option value="stCath">St. Catharines</option>
-            <option value="vineland">Vineland</option>
-          </select>
+          <label htmlFor="region-select" ariaLabel="Select a location"><i class="fas fa-map-marker-alt"></i></label>
+          <div className="custom-select">
+            <select id="region-select" ref={regionEl}>
+              <option value="baseRegion">Niagara Region</option>
+              <option value="beamsville">Beamsville</option>
+              <option value="jordanStn">Jordan Station</option>
+              <option value="lincoln">Lincoln</option>
+              <option value="niagaraFalls">Niagara Falls</option>
+              <option value="notl">Niagara-On-The-Lake</option>
+              <option value="stCath">St. Catharines</option>
+              <option value="vineland">Vineland</option>
+            </select>
+            <i class="fas fa-chevron-down"></i>
+          </div>
 
           <button type="submit">
             Go
