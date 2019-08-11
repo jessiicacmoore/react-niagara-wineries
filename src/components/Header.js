4db6eb11c4c12d6bde2faa39a5ed8c62fc654998
Jessica Moore <jessica.cmoore1@gmail.com>
2019-08-11 15:02:28 -0400

    Functional form that filters region

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,30 @@
-import React from 'react'
+import React, { useRef } from 'react'
 
-const Header = () => {
+const Header = ({handleRegionChange}) => {
+  const regionEl = useRef("baseRgion")
+
   return (
     <header>
-      <h1>Great Wine lives Here.</h1>
+      <div className="wrapper">
+        <h1>Discover the Great Wines<br/>of the Niagara Region.</h1>
+        <form onSubmit={(e) => handleRegionChange(e, regionEl.current.value)}>
+          <label htmlFor="region-select">Region:</label>
+          <select id="region-select" ref={regionEl}>
+            <option value="baseRegion">Niagara Region</option>
+            <option value="beamsville">Beamsville</option>
+            <option value="jordanStn">Jordan Station</option>
+            <option value="lincoln">Lincoln</option>
+            <option value="niagaraFalls">Niagara Falls</option>
+            <option value="notl">Niagara-On-The-Lake</option>
+            <option value="stCath">St. Catharines</option>
+            <option value="vineland">Vineland</option>
+          </select>
+
+          <button type="submit">
+            Go
+          </button>
+        </form>
+      </div>
     </header>
   )
 }
