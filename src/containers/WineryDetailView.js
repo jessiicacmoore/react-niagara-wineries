5aa63617429936b5d84c4dd6a6b5c4bbd9a942b8
Jessica Moore <jessica.cmoore1@gmail.com>
2019-08-12 17:11:29 -0400

    Implemented react carousel

--- src/containers/WineryDetailView.js
+++ src/containers/WineryDetailView.js
@@ -1,6 +1,11 @@
-import React, { useEffect, useState } from 'react'
+import React, {  Fragment, useEffect, useState } from 'react'
 import axios from 'axios'
 import Spinner from '../components/Spinner';
+import placeholderImg from "../img/placeholder.png"
+import { Carousel } from "react-responsive-carousel";
+
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+
 
 require("dotenv").config();
 
@@ -21,22 +26,28 @@ const WineryDetail = (props) => {
         }
       })
       .then(resp => {
-        setWinery(resp.data)
-      })
-      .catch(err => console.log(err));
-
-    axios
-      .get(`https://cors-anywhere.herokuapp.com/${baseUrl}`, {
-        headers: {
-          Authorization: `Bearer ${token}`
-        }
-      })
-      .then(resp => {
         console.log(resp.data)
-        setWineryReviews(resp.data)
+        setWinery(resp.data)
         setLoading(false)
       })
       .catch(err => console.log(err));
+
+    // axios
+    //   .get(`https://cors-anywhere.herokuapp.com/${baseUrl}/reviews`, {
+    //     headers: {
+    //       Authorization: `Bearer ${token}`
+    //     }
+    //   })
+    //   .then(resp => {
+    //     setWineryReviews(resp.data)
+    //     setLoading(false)
+    //     console.log(resp.data)
+    //   })
+    //   .catch(err => console.log(err));
+  }
+
+  const myfunc = () => {
+    console.log(winery.photos[0])
   }
 
   useEffect(() => {
@@ -44,15 +55,37 @@ const WineryDetail = (props) => {
   }, [])
 
   return (
-    <main>
-      { loading ?
-        <div className="loader-container detail-loader">
-          <Spinner />
-        </div>
-        :
-        <h1>{winery.name}</h1>
-      }
-    </main>
+    <Fragment>
+      <nav>
+      </nav>
+      <main>
+        { loading ?
+          <div className="loader-container detail-loader">
+            <Spinner />
+          </div>
+          :
+          <Fragment>
+              <h1 onClick={myfunc}>Click</h1>
+            <div className="img-container">
+              {
+                winery.photos || winery.photos.length > 0 ?
+                <Carousel showThumbs={false}>
+                  {winery.photos.map((photo, i) => {
+                    return (
+                      <div>
+                        <img src={photo} key={i} />
+                      </div>
+                    )
+                  })}
+                </Carousel>
+                :
+                <img src={placeholderImg} alt=""/>
+              }
+            </div>
+          </Fragment>
+        }
+      </main>
+    </Fragment>
   )
 }
 
