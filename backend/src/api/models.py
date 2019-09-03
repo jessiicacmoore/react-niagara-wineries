903d9f7201f80b97ee9e155511ca330c65b064ed
Jessica Moore <jessica.cmoore1@gmail.com>
2019-09-03 15:02:12 -0400

    formatting

--- backend/src/api/models.py
+++ backend/src/api/models.py
@@ -1,35 +1,36 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 class Winery(models.Model):
-	name = models.CharField(max_length=200)
-	image_ref = models.URLField(max_length=200)
-	yelp_id = models.CharField(max_length=200)
-	address = models.CharField(max_length=200)
-	city = models.CharField(max_length=200)
+    name = models.CharField(max_length=200)
+    image_ref = models.URLField(max_length=200)
+    yelp_id = models.CharField(max_length=200)
+    address = models.CharField(max_length=200)
+    city = models.CharField(max_length=200)
 
-	def __str__(self):
-		return self.name
+    def __str__(self):
+        return self.name
 
-	def __repr__(self):
-		return self.name
+    def __repr__(self):
+        return self.name
 
 
 class Pin(models.Model):
-	user = models.ForeignKey(User, related_name="pins", on_delete=models.CASCADE)
-	winery = models.ForeignKey(Winery, related_name="pins", on_delete=models.CASCADE)
-	visited = models.BooleanField(default=False)
+    user = models.ForeignKey(User, related_name="pins", on_delete=models.CASCADE)
+    winery = models.ForeignKey(Winery, related_name="pins", on_delete=models.CASCADE)
+    visited = models.BooleanField(default=False)
 
-	class Meta:
-		unique_together = ('user', 'winery')
+    class Meta:
+        unique_together = ("user", "winery")
 
-	def __str__(self):
-		return f"{self.user} - {self.winery}"
+    def __str__(self):
+        return f"{self.user} - {self.winery}"
 
 
 class Note(models.Model):
-	pin = models.ForeignKey(Pin, related_name="notes", on_delete=models.CASCADE)
-	date_added = models.DateField(auto_now_add=True)
+    pin = models.ForeignKey(Pin, related_name="notes", on_delete=models.CASCADE)
+    date_added = models.DateField(auto_now_add=True)
 
-	def __str__(self):
-		return f"{self.pin} - {self.date_added}"
+    def __str__(self):
+        return f"{self.pin} - {self.date_added}"
