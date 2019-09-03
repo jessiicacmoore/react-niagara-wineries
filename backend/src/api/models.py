ed6aecc9eedca81d7cc9d5ff3dd50130e106d1e6
Jessica Moore <jessica.cmoore1@gmail.com>
2019-09-03 14:13:00 -0400

    Winery model

--- backend/src/api/models.py
+++ backend/src/api/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Winery(models.Model):
+	name = models.CharField(max_length=200)
+	image_ref = models.URLField(max_length=200)
+	yelp_id = models.CharField(max_length=200)
+	address = models.CharField(max_length=200)
+	city = models.CharField(max_length=200)
+
+	def __str__(self):
+		return self.name
+
+	def __repr__(self):
+		return self.name
