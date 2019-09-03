90798d05d71195a8359f7aa860e05eb7a6c9ee6b
Jessica Moore <jessica.cmoore1@gmail.com>
2019-09-03 16:24:26 -0400

    Set up api views

--- backend/src/api/views.py
+++ backend/src/api/views.py
@@ -1,3 +1,19 @@
+from django.contrib.auth.models import User
 from django.shortcuts import render
+from rest_framework import mixins, permissions, viewsets
 
-# Create your views here.
+from .models import *
+from .serializers import *
+
+
+class PinViewSet(viewsets.ModelViewSet):
+    serializer_class = PinSerializer
+
+    def get_queryset(self):
+        return self.request.user.pins.all()
+
+class CreateUserView(mixins.CreateModelMixin, viewsets.GenericViewSet):
+    permission_classes = [permissions.AllowAny]
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+    
